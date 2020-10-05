7b0227b8f0c6b7da97e751d3ae70c25bcddcade2
Mike.Mueller <michael17mueller@gmail.com>
2020-10-05 15:11:26 -0400

    added seperate menu for use mainly on mobile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-scroll';
 import './App.css';
 import Header from './components/Header/Header.component';
@@ -9,60 +9,64 @@ import Contact from './components/Contact/Contact.component';
 import Projects from './components/Projects/Projects.component';
 import Slide from 'react-reveal/Slide';
 
-const App = () => (
-	<div className='App'>
-		<nav className='navbar'>
-			<div className='navbar-menu'>
-				<img src={burger} alt='burger' />
-			</div>
-			<div className='navbar-links'>
-				<Link activeClass='active' to='about' spy={true} smooth={true} duration={1000}>
-					<p className='link'>About</p>
-				</Link>
-				<Link activeClass='active' to='projects' spy={true} smooth={true} duration={1000}>
-					<p className='link'>Projects</p>
-				</Link>
-				<Link activeClass='active' to='contact' spy={true} smooth={true} duration={1000}>
-					<p className='link'>Contact</p>
-				</Link>
-			</div>
-			<div className='navbar-logo'>
-				<Link activeClass='active' to='header' spy={true} smooth={true} duration={1000}>
-					<img src={MM} alt='logo' />
-				</Link>
-			</div>
-			<div className='navbar-spacer' />
-			<div className='navbar-socials'>
-				<p>
-					<i className='devicon-linkedin-plain' />
-				</p>
-				<p>
-					<i className='devicon-github-plain' />
-				</p>
-				<p>@</p>
-			</div>
-		</nav>
-		<Header />
-		<div className='container'>
-			<div className='timeline' />
-			<div className='padding' />
-			<Slide right>
-				<section className='about' id='about'>
-					<About />
-				</section>
-			</Slide>
-			<Slide right>
-				<section className='projects' id='projects'>
-					<Projects />
-				</section>
-			</Slide>
-			<Slide right>
-				<section className='contact' id='contact'>
-					<Contact />
-				</section>
-			</Slide>
-		</div>
-	</div>
-);
+export default function App() {
+	const [ menu, setMenu ] = useState(false);
 
-export default App;
+	useEffect(() => {}, [ menu ]);
+
+	return (
+		<div className='App'>
+			<nav className='navbar'>
+				<div className='navbar-menu'>
+					<img src={burger} alt='burger' onClick={() => setMenu(!menu)} />
+				</div>
+				<div className='navbar-links'>
+					<Link activeClass='active' to='about' spy={true} smooth={true} duration={1000}>
+						<p className='link'>About</p>
+					</Link>
+					<Link activeClass='active' to='projects' spy={true} smooth={true} duration={1000}>
+						<p className='link'>Projects</p>
+					</Link>
+					<Link activeClass='active' to='contact' spy={true} smooth={true} duration={1000}>
+						<p className='link'>Contact</p>
+					</Link>
+				</div>
+				<div className='navbar-logo'>
+					<Link activeClass='active' to='header' spy={true} smooth={true} duration={1000}>
+						<img src={MM} alt='logo' />
+					</Link>
+				</div>
+				<div className='navbar-spacer' />
+				<div className='navbar-socials'>
+					<p>
+						<i className='devicon-linkedin-plain' />
+					</p>
+					<p>
+						<i className='devicon-github-plain' />
+					</p>
+					<p>@</p>
+				</div>
+			</nav>
+			<Header setMenuView={setMenu} menu={menu} />
+			<div className='container'>
+				<div className='timeline' />
+				<div className='padding' />
+				<Slide right>
+					<section className='about' id='about'>
+						<About />
+					</section>
+				</Slide>
+				<Slide right>
+					<section className='projects' id='projects'>
+						<Projects />
+					</section>
+				</Slide>
+				<Slide right>
+					<section className='contact' id='contact'>
+						<Contact />
+					</section>
+				</Slide>
+			</div>
+		</div>
+	);
+}
--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Header.style.scss';
+import { Link } from 'react-scroll';
+import MM from '../../assets/images/MM.png';
+import burger from '../../assets/images/burger.png';
 
-export default function Header() {
+export default function Header({ setMenuView, menu }) {
+	useEffect(() => {}, [ menu ]);
+
 	return (
 		<div className='Header' id='header'>
 			<div className='Header--bg' />
+			{menu ? (
+				<div className='Header--menu'>
+					<div className='Header--menu--head'>
+						<img className='Header--menu--head--logo' src={MM} alt='logo' />
+						<img className='Header--menu--head--btn' src={burger} alt='burger' onClick={() => setMenuView(!menu)} />
+					</div>
+					<Link
+						activeClass='active'
+						to='about'
+						spy={true}
+						smooth={true}
+						duration={1000}
+						onClick={() => setMenuView(!menu)}
+					>
+						<p className='link'>About</p>
+					</Link>
+					<Link
+						activeClass='active'
+						to='projects'
+						spy={true}
+						smooth={true}
+						duration={1000}
+						onClick={() => setMenuView(!menu)}
+					>
+						<p className='link'>Projects</p>
+					</Link>
+					<Link
+						activeClass='active'
+						to='contact'
+						spy={true}
+						smooth={true}
+						duration={1000}
+						onClick={() => setMenuView(!menu)}
+					>
+						<p className='link'>Contact</p>
+					</Link>
+				</div>
+			) : (
+				''
+			)}
 			<h1 className='Header--name1'>Mike</h1>
 			<h1 className='Header--name2'>Mueller</h1>
 			<div className='Header--subtext'>
